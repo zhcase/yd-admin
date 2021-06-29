6189e00a2d0659babe52b98adf08496bbb11bc58
zehua <zehua@zehuadeiMac.local>
2021-06-29 23:14:16 +0800

    init commit

---
+++ src/components/Table/index.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: zeHua
+ * @Date: 2021-06-29 21:48:16
+ * @LastEditors: zeHua
+ * @LastEditTime: 2021-06-29 21:48:17
+ * @FilePath: /base-admin/vue-admin-template/src/components/Table/index.ts
+ */
